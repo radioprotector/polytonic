2706164d3ee375b0378263b67d4466d23a211958
Cate Stock <radioprotector@users.noreply.github.com>
2022-12-24 17:47:46 -0500

    adjust inertia/velocity levels

--- src/ring.lua
+++ src/ring.lua
@@ -4,19 +4,35 @@ import 'CoreLibs/timer'
 import 'glue'
 local C <const> = require 'constants'
 
+local RING_INERTIA <const> = {
+  C.PHI,
+  C.PHI * 1.5,
+  C.PHI * 2,
+  C.PHI * 2.5,
+  C.PHI * 3,
+  C.PHI * 3.25,
+  C.PHI * 3.5,
+  C.PHI * 4
+}
+
 class('Ring').extends()
 
 function Ring:init(layer)
   Ring.super.init(self)
   self.layer = layer
+
   -- Ensure outermost layers have more "inertia"
-  self.inertia = 1.618 ^ (layer / 1.5)
+  self.inertia = RING_INERTIA[self.layer]
   self.angle_rad = 0
   self.angle_velocity = 0
   self.selected = false
-
   if self.layer % 2 == 0 then
-    self.angle_rad = math.pi / 2
+    self.angle_rad = C.HALF_PI
+  end
+
+  if not self.inertia then
+    print("invalid ring! layer #" .. ring.layer)
+    return
   end
 
   -- printTable(self)
@@ -24,7 +40,7 @@ end
 
 function Ring:addVelocity(change_deg)
   -- Convert change radians to change degrees and make it more difficult as the layer moves outward
-  local change_rad = (change_deg / C.TWO_PI) / self.inertia
+  local change_rad = math.rad(change_deg) / self.inertia
 
   self.angle_velocity = self.angle_velocity + change_rad
 
