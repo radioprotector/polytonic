2743ecd0b0fec31516b51f83843a9bde2bb885ad
Cate Stock <radioprotector@users.noreply.github.com>
2022-12-28 01:23:03 -0500

    change background fill based on dissonance

--- src/main.lua
+++ src/main.lua
@@ -1,6 +1,7 @@
 import 'CoreLibs/object'
 import 'CoreLibs/graphics'
 import 'CoreLibs/timer'
+import 'lib/gfxp'
 
 import 'glue'
 import 'ring'
@@ -12,12 +13,49 @@ import 'ui_component'
 local C <const> = require 'constants'
 local gfx <const> = playdate.graphics
 local timer <const> = playdate.timer
+local gfxp <const> = GFXP
+
+local math_floor <const> = math.floor
+local math_fmod <const> = math.fmod
+local math_mapLinear <const> = math.mapLinear
 
 -- Localize key constants
 local RING_COUNT <const> = C.RING_COUNT
 local VELOCITY_PUSH_SINGLE_DEG <const> = C.VELOCITY_PUSH_SINGLE_DEG
 local VELOCITY_PUSH_GLOBAL_DEG <const> = C.VELOCITY_PUSH_GLOBAL_DEG
+local SCREEN_WIDTH <const> = C.SCREEN_WIDTH
+local SCREEN_HEIGHT <const> = C.SCREEN_HEIGHT
 local CENTER_X <const> = C.CENTER_X
+local THIRD_PI <const> = C.THIRD_PI
+local SIXTH_PI <const> = math.pi / 6
+
+-- Dissonance of 0 means that everything is a flat-top polygon.
+-- Dissonance of 100 means that everything is a pointy-top polygon.
+-- Dissonance of 50 means that there's no consistent harmony.
+local DISSONANCE_FILL_LEVELS <const> = {
+  'gray',
+  {0xAA, 0x99, 0xAA, 0x66, 0xAA, 0x99, 0xAA, 0x66},
+  {0xAA, 0x22, 0xAA, 0x88, 0xAA, 0x22, 0xAA, 0x88},
+  {0x22, 0x22, 0x88, 0x88, 0x22, 0x22, 0x88, 0x88},
+  'darkgray',
+  {0x0, 0x22, 0x0, 0x0, 0x0, 0x22, 0x0, 0x0},
+  {0x0, 0x20, 0x0, 0x0, 0x0, 0x2, 0x0, 0x0},
+  {0x0, 0x20, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
+  'black',
+  {0x0, 0x20, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0},
+  {0x0, 0x20, 0x0, 0x0, 0x0, 0x2, 0x0, 0x0},
+  {0x0, 0x22, 0x0, 0x0, 0x0, 0x22, 0x0, 0x0},
+  'darkgray',
+  {0x22, 0x22, 0x88, 0x88, 0x22, 0x22, 0x88, 0x88},
+  {0xAA, 0x22, 0xAA, 0x88, 0xAA, 0x22, 0xAA, 0x88},
+  {0xAA, 0x99, 0xAA, 0x66, 0xAA, 0x99, 0xAA, 0x66},
+  'gray'
+}
+
+---@diagnostic disable-next-line: undefined-field
+local DISSONANCE_FILL_COUNT <const> = table.getsize(DISSONANCE_FILL_LEVELS)
+local DISSONANCE_UPDATE_FRAMES <const> = 15
+local MAX_DISSONANCE = SIXTH_PI * RING_COUNT
 
 -- ====================================
 -- Game state
@@ -35,6 +73,10 @@ local left_key_timer = nil
 local right_key_timer = nil
 local a_key_timer = nil
 local b_key_timer = nil
+local total_dissonance = nil
+local total_dissonance_percentile = nil
+local dissonance_fill = nil
+local dissonance_fill_frames = DISSONANCE_UPDATE_FRAMES
 
 local function loadGame()
   math.randomseed(playdate.getSecondsSinceEpoch()) -- seed for math.random
@@ -196,9 +238,23 @@ local function updateGame()
     pushSelectedRing(-change)
   end
 
-  -- Update each ring
+  -- Update each ring, and calculate total dissonance by measuring
+  -- variance from a 0/60/120/180/240/300/360-degree position,
+  -- since all of those appear the same
+  total_dissonance = 0
+  total_dissonance_percentile = 0
+
   for _, value in pairs(RINGS) do
     value:update()
+
+    -- Calculate dissonance
+    -- If we're more than 30 degrees, we're actually closer to the next angle
+    local ring_dissonance = math_fmod(value.angle_rad, THIRD_PI)
+    if ring_dissonance > SIXTH_PI then
+      total_dissonance += (THIRD_PI - ring_dissonance)
+    else
+      total_dissonance += ring_dissonance
+    end
   end
 
   -- Update the display components associated with the rings
@@ -213,12 +269,26 @@ local function updateGame()
 
   -- Update the UI component
   UI_COMPONENT:update()
+
+  -- Calculate the total dissonance percentile
+  total_dissonance_percentile = total_dissonance / MAX_DISSONANCE
+
+  -- Increment the number of dissonance fill frames
+  dissonance_fill_frames += 1
+
+  -- Calculate the dissonance fill if we have time
+  if dissonance_fill_frames > DISSONANCE_UPDATE_FRAMES then
+    local dissonance_level <const> = math_floor(math_mapLinear(total_dissonance_percentile, 0.0, 1.0, 0, DISSONANCE_FILL_COUNT - 1)) + 1
+
+    dissonance_fill = DISSONANCE_FILL_LEVELS[dissonance_level]
+    dissonance_fill_frames = 0
+  end
 end
 
 local function drawGame()
-  -- Start with a black background
-  gfx.setBackgroundColor(gfx.kColorBlack)
-  gfx.clear()
+  -- Start with a background fill
+  gfxp.set(dissonance_fill)
+  gfx.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
 
   -- Ensure all rings are drawn.
   -- Go in reverse order to render the smallest last.
@@ -228,6 +298,16 @@ local function drawGame()
 
   -- Render the UI component
   UI_COMPONENT:draw()
+
+  -- -- DEBUG TEXT
+  -- gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
+
+  -- for i = RING_COUNT, 1, -1 do
+  --   gfx.drawText(string.format('%.0f', math.deg(RINGS[i].angle_rad)), 0, SCREEN_HEIGHT - (20 * (i + 1)))
+  -- end
+
+  -- gfx.drawText(string.format('%.0f', math.deg(total_dissonance)) ..
+  --   ' dis (' .. string.format('%.0f', total_dissonance_percentile * 100) .. '%)' , 0, SCREEN_HEIGHT - 20)
 end
 
 loadGame()
